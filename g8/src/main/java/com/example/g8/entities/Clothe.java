2201712c7392767888328444c971025c9995883c
edwingalviz <91507069+edwingalviz@users.noreply.github.com>
2021-11-28 09:32:49 -0500

    reto 2

---
+++ g8/src/main/java/com/example/g8/entities/Clothe.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.g8.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author bmao9
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "clothe")
+public class Clothe{
+   
+    @Id
+    private String reference;
+    private String category;
+    private String size;
+    private String description;
+    private String availability;
+    private Integer price;
+    private Integer quantity;
+    private String photography;  
+}
+
---
+++ g8/src/main/java/com/example/g8/entities/User.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.g8.entities;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author bmao9
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "user")
+public class User{
+    
+    @Id
+    private Integer id;
+    private String identification;
+    private String name;
+    private String address;
+    private String cellPhone;
+    private String email;
+    private String password;
+    private String zone;
+    private String type;
+
+    
+    
+}
